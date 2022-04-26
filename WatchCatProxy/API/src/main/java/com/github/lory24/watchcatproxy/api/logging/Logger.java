f897da88ac6b7fa878c79e08b6f9b63787408742
LoRy24 <lorytherock24tutorial@gmail.com>
2022-04-26 19:30:40 +0200

    Optimized the logger

--- WatchCatProxy/API/src/main/java/com/github/lory24/watchcatproxy/api/logging/Logger.java
+++ WatchCatProxy/API/src/main/java/com/github/lory24/watchcatproxy/api/logging/Logger.java
@@ -1,26 +1,14 @@
 package com.github.lory24.watchcatproxy.api.logging;
 
-import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @SuppressWarnings("unused")
-public class Logger implements Runnable {
-
-    /**
-     * The log object. Used to store pending logs into the logs queue
-     *
-     * @param message The log message
-     * @param level   The log level
-     * @param store   If store the log
-     */
-    public record Log(@Getter String message, @Getter LogLevel level, @Getter boolean store) {}
+public class Logger {
 
     /**
      * The file where all the log of this server session is going to be saved
@@ -31,11 +19,6 @@ public class Logger implements Runnable {
      * The name for the logger. It's also used as prefix for the logger
      */
     private final String loggerName;
-
-    /**
-     * The pending logs queue
-     */
-    private final List<Log> pendingQueue = new ArrayList<>();
 
     /**
      * The current saved log file content. Used when writing data into the file
@@ -50,40 +33,6 @@ public class Logger implements Runnable {
     public Logger(File logFile, String loggerName) {
         this.logFile = logFile;
         this.loggerName = loggerName;
-        Thread loggerThread = new Thread(this);
-        loggerThread.setPriority(2);
-        loggerThread.start();
-    }
-
-    /**
-     * Run the logger runnable. This is going to send all the logs whenever there is a new
-     * one in the queue
-     */
-    @SuppressWarnings("deprecation")
-    @Override
-    public void run() {
-        // Run this loop while the thread is running
-        while (!Thread.currentThread().isInterrupted()) {
-            if (pendingQueue.size() == 0) continue;
-
-            // Create the log string
-            // Format: [hh:mm:ss] [prefix] message
-            Date date = new Date();
-            Log logObject = pendingQueue.get(0);
-            String logString = String.format("[ %02.0f:%02.0f:%02.0f ] [ %s - %s ] %s",
-                    (float) date.getHours(), (float) date.getMinutes(), (float) date.getSeconds(),
-                    loggerName,
-                    logObject.level,
-                    logObject.message
-            );
-
-            // Log the string
-            System.out.println(logObject.level().getLogColor() + logString);
-            if (logObject.store()) storeLog(logString);
-
-            // Remove obj from queue
-            pendingQueue.remove(0);
-        }
     }
 
     /**
@@ -107,8 +56,7 @@ public class Logger implements Runnable {
      * @param message The message to log
      */
     public void log(LogLevel level, String message) {
-        Log errorLog = new Log(message, level, true);
-        this.pendingQueue.add(errorLog);
+        this.log(level, message, true);
     }
 
     /**
@@ -118,8 +66,16 @@ public class Logger implements Runnable {
      * @param store If store the msg
      */
     public void log(LogLevel level, String message, boolean store) {
-        Log errorLog = new Log(message, level, store);
-        this.pendingQueue.add(errorLog);
+        // Create the log string
+        // Format: [hh:mm:ss] [prefix] message
+        Date date = new Date();
+        String logString = String.format("[ %02.0f:%02.0f:%02.0f ] [ %s - %s ] %s",
+                (float) date.getHours(), (float) date.getMinutes(), (float) date.getSeconds(), loggerName, level, message
+        );
+
+        // Log the string
+        System.out.println(level.getLogColor() + logString);
+        if (store) storeLog(logString);
     }
 
     /**
